6478e4700565f0294bb30f48d714916aaec4cdda
samuraiwarriornyc-tech <samuraiwarrior.nyc@gmail.com>
2026-07-16 11:33:56 -0400

    Update customizer.sh

--- devices/gk7202v300_lite_ipg-g3-wr/general/overlay/usr/share/openipc/customizer.sh
+++ devices/gk7202v300_lite_ipg-g3-wr/general/overlay/usr/share/openipc/customizer.sh
@@ -2,7 +2,7 @@
 
 fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/gk7202v300_lite_ipg-g3-wr-nor.tgz'
 
-fw_setenv wlandev atbm603x-generic-usb
+fw_setenv wlandev atbm6012b-generic-usb
 fw_setenv sensor jxh63_i2c_720p
 
 exit 0
